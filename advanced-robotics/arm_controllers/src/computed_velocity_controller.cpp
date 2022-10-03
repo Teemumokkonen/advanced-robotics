a66216dfe70f1ddbe43f90f10fcaa003acc3fdf0
Teemu Mökkönen <teemu.mokkonen@tuni.fi>
2022-10-03 13:23:08 +0300

    Add topic publishing for robot states and desired states

--- advanced-robotics/arm_controllers/src/computed_velocity_controller.cpp
+++ advanced-robotics/arm_controllers/src/computed_velocity_controller.cpp
@@ -472,10 +472,109 @@ class ComputedVelocityController : public controller_interface::Controller<hardw
         }
 
         //print_state();
+        save_data();
     }
 
     void stopping(const ros::Time &time)
     {
+    }
+
+    void save_data()
+    {
+        // 1
+        // Simulation time (unit: sec)
+        SaveData_[0] = t;
+
+        // Desired position in joint space (unit: rad)
+        SaveData_[1] = qd_(0);
+        SaveData_[2] = qd_(1);
+        SaveData_[3] = qd_(2);
+        SaveData_[4] = qd_(3);
+        SaveData_[5] = qd_(4);
+        SaveData_[6] = qd_(5);
+
+        // Desired velocity in joint space (unit: rad/s)
+        SaveData_[7] = qd_dot_(0);
+        SaveData_[8] = qd_dot_(1);
+        SaveData_[9] = qd_dot_(2);
+        SaveData_[10] = qd_dot_(3);
+        SaveData_[11] = qd_dot_(4);
+        SaveData_[12] = qd_dot_(5);
+
+        // Desired acceleration in joint space (unit: rad/s^2)
+        SaveData_[13] = qd_ddot_(0);
+        SaveData_[14] = qd_ddot_(1);
+        SaveData_[15] = qd_ddot_(2);
+        SaveData_[16] = qd_ddot_(3);
+        SaveData_[17] = qd_ddot_(4);
+        SaveData_[18] = qd_ddot_(5);
+
+        // Actual position in joint space (unit: rad)
+        SaveData_[19] = q_(0);
+        SaveData_[20] = q_(1);
+        SaveData_[21] = q_(2);
+        SaveData_[22] = q_(3);
+        SaveData_[23] = q_(4);
+        SaveData_[24] = q_(5);
+
+        // Actual velocity in joint space (unit: rad/s)
+        SaveData_[25] = qdot_(0);
+        SaveData_[26] = qdot_(1);
+        SaveData_[27] = qdot_(2);
+        SaveData_[28] = qdot_(3);
+        SaveData_[29] = qdot_(4);
+        SaveData_[30] = qdot_(5);
+
+        // Error position in joint space (unit: rad)
+        SaveData_[31] = e_(0);
+        SaveData_[32] = e_(1);
+        SaveData_[33] = e_(2);
+        SaveData_[34] = e_(3);
+        SaveData_[35] = e_(4);
+        SaveData_[36] = e_(5);
+
+        // Error velocity in joint space (unit: rad/s)
+        SaveData_[37] = e_dot_(0);
+        SaveData_[38] = e_dot_(1);
+        SaveData_[39] = e_dot_(2);
+        SaveData_[40] = e_dot_(3);
+        SaveData_[41] = e_dot_(4);
+        SaveData_[42] = e_dot_(5);
+
+        // Error intergal value in joint space (unit: rad*sec)
+        SaveData_[43] = e_int_(0);
+        SaveData_[44] = e_int_(1);
+        SaveData_[45] = e_int_(2);
+        SaveData_[46] = e_int_(3);
+        SaveData_[47] = e_int_(4);
+        SaveData_[48] = e_int_(5);
+
+        // 2
+        msg_qd_.data.clear();
+        msg_q_.data.clear();
+        msg_e_.data.clear();
+
+        msg_SaveData_.data.clear();
+
+        // 3
+        for (int i = 0; i < n_joints_; i++)
+        {
+            msg_qd_.data.push_back(qd_(i));
+            msg_q_.data.push_back(q_(i));
+            msg_e_.data.push_back(e_(i));
+        }
+
+        for (int i = 0; i < SaveDataMax; i++)
+        {
+            msg_SaveData_.data.push_back(SaveData_[i]);
+        }
+
+        // 4
+        pub_qd_.publish(msg_qd_);
+        pub_q_.publish(msg_q_);
+        pub_e_.publish(msg_e_);
+
+        pub_SaveData_.publish(msg_SaveData_);
     }
 
     void print_state()
